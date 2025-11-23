c1f67710abeac186c6ffef78095b3e6cad3f27f6
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2025-11-23 12:26:16 +0530

    Update main.tf

---
+++ vpc_primary/main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "this" {
+  cidr_block = "10.10.0.0/16"
+  tags = { Name = "vault-primary-vpc" }
+}
+
+resource "aws_subnet" "private" {
+  count = 3
+  vpc_id = aws_vpc.this.id
+  cidr_block = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index)
+  availability_zone = "${var.region}${substr("abc", count.index, 1)}"
+
+  tags = { Name = "vault-primary-private-${count.index}" }
+}
+
+output "vpc_id"      { value = aws_vpc.this.id }
+output "vpc_cidr"    { value = aws_vpc.this.cidr_block }
+output "private_subnets" { value = aws_subnet.private[*].id }
