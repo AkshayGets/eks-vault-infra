7ee21b1d1794e7d63bc41b833c848f4d5c36d8ed
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2025-11-23 13:00:53 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "primary_vpc_id" {
 output "secondary_vpc_id" {
   value = module.vpc_secondary.vpc_id
 }
-
-output "primary_eks_cluster_name" {
-  value = module.eks_primary.cluster_name
-}
-
-output "secondary_eks_cluster_name" {
-  value = module.eks_secondary.cluster_name
-}
