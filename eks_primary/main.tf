46fec25f95dae9a4938240fe74816b2395915675
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2025-11-23 12:47:48 +0530

    Update main.tf

--- eks_primary/main.tf
+++ eks_primary/main.tf
@@ -1,5 +1,7 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "21.9.0"
+
   name            = "vault-primary-eks"
   version         = "1.29"
 
@@ -30,4 +32,5 @@ resource "aws_security_group_rule" "allow_replication" {
 output "cluster_name"         { value = module.eks.cluster_name }
 output "cluster_endpoint"     { value = module.eks.cluster_endpoint }
 output "cluster_security_group_id" { value = module.eks.cluster_security_group_id }
+
 
